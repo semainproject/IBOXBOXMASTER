87b3022f9cc7405f1ae3862971cbfa69f8aa1fd2
Kittaporn <kittaporn.j@ku.th>
2017-11-09 05:06:27 +0700

    gungungn//endday2

--- app/src/main/java/com/example/kittaporn/iboxbox/MainActivity.java
+++ app/src/main/java/com/example/kittaporn/iboxbox/MainActivity.java
@@ -16,6 +16,10 @@ import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import org.eazegraph.lib.charts.StackedBarChart;
+import org.eazegraph.lib.models.BarModel;
+import org.eazegraph.lib.models.StackedBarModel;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -25,7 +29,36 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        StackedBarChart mStackedBarChart = (StackedBarChart) findViewById(R.id.stackedbarchart);
 
+        StackedBarModel s1 = new StackedBarModel("12.4");
+
+        s1.addBar(new BarModel(2.3f, 0xFF63CBB0));
+        s1.addBar(new BarModel(2.3f, 0xFF56B7F1));
+        s1.addBar(new BarModel(2.3f, 0xFFCDA67F));
+
+        StackedBarModel s2 = new StackedBarModel("13.4");
+        s2.addBar(new BarModel(1.1f, 0xFF63CBB0));
+        s2.addBar(new BarModel(2.7f, 0xFF56B7F1));
+        s2.addBar(new BarModel(0.7f, 0xFFCDA67F));
+
+        StackedBarModel s3 = new StackedBarModel("14.4");
+
+        s3.addBar(new BarModel(2.3f, 0xFF63CBB0));
+        s3.addBar(new BarModel(2.f, 0xFF56B7F1));
+        s3.addBar(new BarModel(3.3f, 0xFFCDA67F));
+
+        StackedBarModel s4 = new StackedBarModel("15.4");
+        s4.addBar(new BarModel(1.f, 0xFF63CBB0));
+        s4.addBar(new BarModel(4.2f, 0xFF56B7F1));
+        s4.addBar(new BarModel(2.1f, 0xFFCDA67F));
+
+        mStackedBarChart.addBar(s1);
+        mStackedBarChart.addBar(s2);
+        mStackedBarChart.addBar(s3);
+        mStackedBarChart.addBar(s4);
+
+        mStackedBarChart.startAnimation();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
