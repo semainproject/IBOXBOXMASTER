a8813d19d0b3d3158bf4e94daf35ad75790920ee
Kittaporn <kittaporn.j@ku.th>
2017-11-08 16:57:34 +0700

    from gundddd

--- app/src/main/java/com/example/kittaporn/iboxbox/LoginActivity.java
+++ app/src/main/java/com/example/kittaporn/iboxbox/LoginActivity.java
@@ -1,26 +1,28 @@
 package com.example.kittaporn.iboxbox;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-    TextView emailTx,passTx;
+    EditText emailTx,passTx;
     Button signInBtn,signUpBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        emailTx = (TextView) findViewById(R.id.emailTx);
-        passTx = (TextView) findViewById(R.id.passTx);
+        emailTx = (EditText) findViewById(R.id.emailTx);
+        passTx = (EditText) findViewById(R.id.passTx);
         signInBtn = (Button) findViewById(R.id.signInBtn);
         signUpBtn = (Button) findViewById(R.id.signUpbtn);
         signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ////////////////
+                //Intent i = new Intent(LoginActivity.this,MainActivity.class);
             }
         });
         signInBtn.setOnClickListener(new View.OnClickListener() {
